6e9ce0f71f32b1a3501dde9b1f0c82a2b7ee5ad6
qutidu <alfrcoug99@gmail.com>
2026-04-23 16:55:13 +0200

    Add files via upload

---
+++ temperature.php
@@ -0,0 +1,23 @@
+<?php  require('include/connect.php'); ?>
+<?php  require('include/fonctions.php'); ?>
+<?php
+session_start();
+
+// 1. Sécurité
+if (empty($_SESSION['mailU'])) {
+    header("Location: connexion.php");
+    exit();
+}
+
+// 2. Récupération du nom de la ruche
+$nomRuche = isset($_GET['nom']) ? $_GET['nom'] : "Ruche inconnue";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>HoneyZzz - Température <?php echo $nomRuche; ?></title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
