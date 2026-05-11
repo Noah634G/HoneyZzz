1308c453057442273e79381aac49f5ab350621ca
olenadanielle <danielle.tsirenge@gmail.com>
2026-05-11 11:31:07 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,29 @@
+<?php
+require('include/connect.php');
+require('include/fonctions.php');
+
+session_start();
+
+if ($_SESSION['role'] != 'admin') {
+    header("Location: connexion.php");
+    exit();
+}
+
+$connexion = mysqli_connect("p:".SERVEUR, NOM, PASSE, BD);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Administration</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Gestion des ruches</h1>
+
+<?php AfficherRuchesAdmin($connexion); ?>
+
+</body>
+</html>
